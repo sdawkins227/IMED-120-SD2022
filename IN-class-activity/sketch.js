4836af89ca22b5203f148ef2c94378650234dacc
Sydney Dawkins <97813386+sdawkins227@users.noreply.github.com>
2022-03-10 13:59:23 -0500

    week9-homework

--- IN-class-activity/sketch.js
+++ IN-class-activity/sketch.js
@@ -1,33 +1,13 @@
 function setup() {
   // put setup code here
-  createCanvas(800, 800);
-  background('white');
+  createCanvas(windowWidth, windowHeight);
+    background ('lavender')
 }
 
 function draw() {
-  // put drawing code here
-  fill('red');
-  rect(30, 60, 60, 60);
-  rect(30, 150, 60, 60);
-  rect(125, 60, 150, 60);
-  rect(125, 150, 150, 60);
-  
-  fill('yellow');
-  rect(30, 300, 300, 200);
-  
-  fill('green');
-  triangle(30, 300, 250, 300, 30, 500,);
+        rect(30, 20, 55, 55, 20);
+    
+    
+    }
+    
 
-  fill('red');
-  triangle(330, 300, 110, 500, 330, 500);
-  
-  
-  
-  
-  
-  
-  
-  ;
-
-
-}
